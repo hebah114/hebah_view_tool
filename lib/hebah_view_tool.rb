ef83b0baaf1770510bd6646c9678681ab92b86aa
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 14:58:14 +0000

    Gem initial commit

---
+++ lib/hebah_view_tool.rb
@@ -0,0 +1,6 @@
+require "hebah_view_tool/version"
+require "hebah_view_tool/renderer"
+
+module HebahViewTool
+  # Your code goes here...
+end
---
+++ lib/hebah_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HebahViewTool
+  VERSION = "0.1.0"
+end
